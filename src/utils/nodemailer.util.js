574858f11aa50c509a589b1a9bb3e5280cb26663
Aritra <aritra1521@gmail.com>
2025-03-19 23:02:17 +0530

    added

--- src/utils/nodemailer.util.js
+++ src/utils/nodemailer.util.js
@@ -1,5 +1,4 @@
-const nodemailer = require('nodemailer');
-
+const nodemailer = require("nodemailer");
 
 // const transporter = nodemailer.createTransport({
 //     service: 'gmail',
@@ -10,40 +9,36 @@ const nodemailer = require('nodemailer');
 // });
 
 const transporter = nodemailer.createTransport({
-    host: 'smtpout.secureserver.net', // GoDaddy SMTP server
-    port: 465, // Secure SMTP port
-    secure: true, // Use SSL/TLS
-    auth: {
-      user: "info@hairsncares.com", // Your GoDaddy email
-      pass: 'qb<]4$W3253=9cE', // Your GoDaddy email password
-    },
-  });
-  
+  host: "smtpout.secureserver.net", // GoDaddy SMTP server
+  port: 465, // Secure SMTP port
+  secure: true, // Use SSL/TLS
+  auth: {
+    user: "info@hairsncares.com", // Your GoDaddy email
+    pass: "H@irs#321", // Your GoDaddy email password
+  },
+});
 
 const sendEmail = async (to, subject, text) => {
-    const mailOptions = {
-        from: 'info@hairsncares.com',
-        to,
-        subject,
-        text
-    };
+  const mailOptions = {
+    from: "info@hairsncares.com",
+    to,
+    subject,
+    text,
+  };
 
-    await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 };
 
-const sendEmailTemplate = async (to, subject,emailHtml) => {
-    const mailOptions = {
-        from: 'info@hairsncares.com',
-        to,
-        subject,
-        html: emailHtml,
-
-    };
-    console.log("ojeroj",mailOptions)
-    let re = await transporter.sendMail(mailOptions);
-    return re;
+const sendEmailTemplate = async (to, subject, emailHtml) => {
+  const mailOptions = {
+    from: "info@hairsncares.com",
+    to,
+    subject,
+    html: emailHtml,
+  };
+  console.log("ojeroj", mailOptions);
+  let re = await transporter.sendMail(mailOptions);
+  return re;
 };
 
-
-module.exports = { sendEmail,sendEmailTemplate};
-
+module.exports = { sendEmail, sendEmailTemplate };
