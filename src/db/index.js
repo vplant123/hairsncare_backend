7c0159f77450a4ef63605d208bda69c6f9a86b87
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2025-05-30 11:17:51 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,12 @@ const connectDB = async () => {
   try {
     // const connectionInstance = await mongoose.connect("mongodb+srv://hairsncares:Kz3o8JfxuxxQFHj7@hairsncares.d3wpd.mongodb.net/HairsNCare",
     // );
+    // const connectionInstance = await mongoose.connect(
+    //   "mongodb://127.0.0.1:27017/HairsNCare?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1"
+    // );
+
     const connectionInstance = await mongoose.connect(
-      "mongodb://127.0.0.1:27017/HairsNCare?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1"
+      "mongodb://develop:911@Admin007@147.79.68.31:27017/myappdb?authSource=admin"
     );
 
     console.log(
