f329427f5fc9bf9589b81c552a1d9301e069ace4
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2025-05-30 11:48:32 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // Use environment variables to securely store sensitive data
-    const dbUri = "mongodb://develop:911%40Admin007@147.79.68.31:27017/myappdb";
+    const dbUri =
+      "mongodb://develop:911@Admin007@127.0.0.1:27017/myappdb?authSource=myappdb";
+
+    // const dbUri = "mongodb://develop:911%40Admin007@147.79.68.31:27017/myappdb";
 
     // Connecting to MongoDB (for both local and remote MongoDB instances)
     const connectionInstance = await mongoose.connect(dbUri, {
